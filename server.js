5c5cd9b40bcf111786dbf46269090b0c0e9ab5c8
Iryna Murashkovska <murira24@gmail.com>
2023-01-30 10:17:14 +0100

    hextobase64
    Add hextobase64 method

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
-const jsonServer = require("json-server");
-const cors = require("cors");
-const path = require("path");
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults();
+const express = require('express')
+const app = express()
+const CryptoJS = require("crypto-js");
+var encodeURISafe = require('encodeuri-safe');
 
-server.use(cors());
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
-server.use(router);
+app.get('/calculate/:num1/:num2', (req, res) => {
+    let num1 = req.params.num1;
+    let num2 = req.params.num2;
+    let result = Number(num1) + Number(num2);
+    console.log('num1 ' + num1)
+    console.log('num2 ' + num2)
+    console.log('Sum ' + result)
+    res.send({result});
+})
 
-const PORT = process.env.PORT || 3000;
+app.get('/hextobase64/:hmac', (req, res) => {
+  let hmac = req.params.hmac;
+  console.log("hmac " + hmac);
+  const hmacEncoded = hexToBase64(hmac.toString());
+  let result = hmacEncoded;
+  console.log('hmacEncoded ' + hmacEncoded)
+  res.send({result});
+})
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
-});
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
+})
+
+function hexToBase64(hexstring) {
+  console.log("hexToBase64");
+  return btoa(hexstring.match(/\w{2}/g).map(function(a) {
+      return String.fromCharCode(parseInt(a, 16));
+  }).join(""));
+}
